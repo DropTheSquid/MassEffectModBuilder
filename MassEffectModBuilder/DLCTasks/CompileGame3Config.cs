9d2b66fa01c87da1897688f4a9fcbacf9c784999
DropTheSquid <DropTheSquid17@gmail.com>
2024-01-25 18:06:16 -0800

    added code to compile ME3/LE3 config. untested (for now)

---
+++ MassEffectModBuilder/DLCTasks/CompileGame3Config.cs
@@ -0,0 +1,17 @@
+﻿using LegendaryExplorerCore.Coalesced;
+using System.Xml.Linq;
+
+namespace MassEffectModBuilder.DLCTasks
+{
+    public record class CompileGame3Config(string XmlFilePath) : ModBuilderTask
+    {
+        public void RunModTask(ModBuilderContext context)
+        {
+            var xmldocument = XDocument.Load(XmlFilePath);
+            var rootElement = xmldocument.Root;
+            
+            var dest = Path.Combine(context.CookedPCConsoleFolder, rootElement.Attribute(@"name").Value);
+            CoalescedConverter.ConvertToBin(XmlFilePath, dest);
+        }
+    }
+}
